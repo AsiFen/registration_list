6d9060a4eb7f0b125f41dd85f91370f6e7065a43
Asisipho <missfente@gmail.com>
2023-05-08 15:13:54 +0200

    2nd commit

--- registration.js
+++ registration.js
@@ -1,48 +1,51 @@
-var user_registrationElem = document.querySelector('.registration_number')
+var user_registrationElem = document.querySelector('.reg_number')
 var btnAdd = document.querySelector(".btnAdd")
+var container = document.querySelector('.container')
 const dropdownSlected = document.querySelector("select");
+
+
 let createDiv = ''
 let createSpan = ''
 if (localStorage['user-registration']) {
     var storeRegistrations = localStorage['user-registration']
 }
+
+function displayRegistrations(registration) {
+    createDiv = document.createElement('div')
+    createSpan = document.createElement('SPAN')
+    createSpan.textContent = registration
+    createDiv.appendChild(createSpan)
+    container.appendChild(createDiv)
+    createDiv.classList.add('displayReg')
+}
+
 var factory_instance = FactoryRegistration()
 
 btnAdd.addEventListener('click', function () {
+
     var user_registration = user_registrationElem.value.trim()
-    const selectedChoice = dropdownSlected.value;
 
     if (user_registration !== '') {
-        var x = factory_instance.addRegistrations(user_registration)
+        factory_instance.addRegistrations(user_registration)
         localStorage['user-registration'] = factory_instance.getRegistrations()
-        createDiv = document.createElement('div')
-        createSpan = document.createElement('SPAN')
-        createSpan.textContent = user_registration
-        createDiv.appendChild(createSpan)
-        document.body.appendChild(createDiv)
-        createDiv.classList.add('displayReg')
-
-        // console.log(selectedChoice);
-        // regItem.textContent = ();
-        // reg_div.appendChild(regItem)
-        // document.body.appendChild(reg_div)
-        // reg_div.classList.add('displayReg')
+        displayRegistrations(user_registration)
     }
 })
 dropdownSlected.addEventListener('change', (event) => {
-        document.body.removeChild(createDiv)
-
+    container.innerHTML = ''
     const {
         value,
     } = event.target.options[event.target.selectedIndex]
-    var x = factory_instance.selectTown(value)
-    console.log(x)
-    console.log(value);
+
+    var selectedTownList = factory_instance.selectTown(value)
+
+    console.log(selectedTownList)
     console.log(localStorage['user-registration']);
-    createDiv = document.createElement('div')
-    createSpan = document.createElement('SPAN')
-    createSpan.textContent = x
-    createDiv.appendChild(createSpan)
-    document.body.appendChild(createDiv)
-    createDiv.classList.add('displayReg')
+
+    var selectedTownListItem = ''
+    for (var i = 0; i < selectedTownList.length; i++) {
+        selectedTownListItem = selectedTownList[i]
+    displayRegistrations(selectedTownListItem)
+    }
+
 })
--- registration_numbers.factory.js
+++ registration_numbers.factory.js
@@ -13,9 +13,12 @@ function FactoryRegistration() {
         selectedItem = []
         for (var i = 0; i < registration_list.length; i++) {
             var firstTwoChars = registration_list[i].charAt(0) + registration_list[i].charAt(1)
-            let reg_indicator = registration_list[i].split(' ')
             if (dropdown_value == firstTwoChars) {
                 selectedItem.push(registration_list[i])
+            }
+            if(dropdown_value ==  'allTowns'){
+                selectedItem.push(registration_list[i])
+
             }
         }
         return selectedItem
